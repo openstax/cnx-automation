040dffb5fbd7562f363922c070e8d90466b338b8
m1yag1 <mike.arbelaez@rice.edu>
2018-04-16 13:49:53 -0500

    added subject list test

--- pages/browse.py
+++ pages/browse.py
@@ -11,6 +11,8 @@ class Browse(Base):
     _search_locator = (By.ID, 'find-content-input')
     _main_content_locator = (By.ID, 'main-content')
     _advanced_search_locator = (By.CLASS_NAME, 'advanced-search')
+    _browse_content_locator = (By.CLASS_NAME, 'browse-content')
+    _subject_list_locator = (By.CSS_SELECTOR, '.browse-content > ul > li')
 
     def wait_for_page_to_load(self):
         self.wait.until(lambda s: self.is_element_displayed(*self._main_content_locator))
@@ -24,5 +26,10 @@ class Browse(Base):
     def is_advanced_search_button_displayed(self):
         return self.is_element_displayed(*self._advanced_search_locator)
 
-    class Subjects(Region):
+    @property
+    def subject_list(self):
+        items = self.find_elements(*self._subject_list_locator)
+        return [Browse.Subject(self, el) for el in items]
+
+    class Subject(Region):
         pass
--- tests/test_browse.py
+++ tests/test_browse.py
@@ -19,3 +19,16 @@ def test_search_items_are_displayed(base_url, selenium):
     # THEN The search bar and the advanced search button is displayed
     assert browse_page.is_search_input_displayed
     assert browse_page.is_advanced_search_button_displayed
+
+
+@pytest.mark.nondestructive
+def test_subject_categories_load(base_url, selenium):
+    # GIVEN the base_url and Selenium driver
+
+    # When The homepage URL is fully loaded,
+    #      and the browse link in the navbar is clicked
+    page = Home(selenium, base_url).open()
+    browse_page = page.header.click_browse()
+
+    # Then The subject categories are loaded
+    assert len(browse_page.subject_list) > 0
