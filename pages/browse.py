b08c6119c00defae98632372d3fd67d02bc17799
m1yag1 <mike.arbelaez@rice.edu>
2018-04-16 12:51:08 -0500

    Adding tests for the clicking Search link on the Home page
    * test for the search box and the advanced search button.

---
+++ pages/browse.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from pypom import Region
+from selenium.webdriver.common.by import By
+
+from pages.base import Base
+
+
+class Browse(Base):
+    _search_locator = (By.ID, 'find-content-input')
+    _main_content_locator = (By.ID, 'main-content')
+    _advanced_search_locator = (By.CLASS_NAME, 'advanced-search')
+
+    def wait_for_page_to_load(self):
+        self.wait.until(lambda s: self.is_element_displayed(*self._main_content_locator))
+        return self
+
+    @property
+    def is_search_input_displayed(self):
+        return self.is_element_displayed(*self._search_locator)
+
+    @property
+    def is_advanced_search_button_displayed(self):
+        return self.is_element_displayed(*self._advanced_search_locator)
+
+    class Subjects(Region):
+        pass
---
+++ tests/test_browse.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.home import Home
+
+
+@pytest.mark.nondestructive
+def test_search_items_are_displayed(base_url, selenium):
+    # GIVEN The base_url and Selenium driver
+
+    # WHEN The home page URL is fully loaded,
+    #      and the browse link in the navbar is clicked
+    page = Home(selenium, base_url).open()
+    browse_page = page.header.click_browse()
+
+    # THEN The search bar and the advanced search button is displayed
+    assert browse_page.is_search_input_displayed
+    assert browse_page.is_advanced_search_button_displayed
