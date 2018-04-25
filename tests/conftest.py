acbafdde2fca747e35587a967e41ac233ac4b40b
Kerwin So <kerwinso@rice.edu>
2018-04-25 10:31:10 -0500

    add blank lines for flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,7 @@ def pytest_addoption(parser):
     parser.addoption('--runslow', action='store_true',
                      default=False, help='run slow tests')
 
+
 def pytest_collection_modifyitems(config, items):
     if config.getoption('--runslow'):
         # --runslow given in cli: do not skip slow tests
@@ -27,6 +28,7 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if 'slow' in item.keywords:
             item.add_marker(skip_slow)
+
 
 @pytest.fixture
 def chrome_options(chrome_options, pytestconfig):
