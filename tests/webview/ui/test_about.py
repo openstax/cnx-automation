e7a33490038742f16bcc1b4ed2b03eed1375b78b
Dante Soares <dante.m.soares@rice.edu>
2018-05-24 12:13:04 -0500

    Added test about About Us content

--- tests/webview/ui/test_about.py
+++ tests/webview/ui/test_about.py
@@ -14,20 +14,35 @@ def test_about_us_links_are_positioned_properly(base_url, selenium):
     home = Home(selenium, base_url).open()
 
     # WHEN the About Us link in the navbar is clicked
-    about_us_page = home.header.click_about_us()
+    about_us = home.header.click_about_us()
 
     # THEN the about us and contact links are same size
     # and vertically stacked on the left of the page
-    about_us = about_us_page.about_us_link
-    contact = about_us_page.contact_link
+    about_us_link = about_us.about_us_link
+    contact_link = about_us.contact_link
     # Same size
-    assert about_us.size == contact.size
+    assert about_us_link.size == contact_link.size
     # Stacked:
     # Same x
-    assert about_us.location['x'] == contact.location['x']
+    assert about_us_link.location['x'] == contact_link.location['x']
     # Similar y
-    assert (about_us.location['y'] + about_us.size['height'] <
-            contact.location['y'] <
-            about_us.location['y'] + 2 * about_us.size['height'])
+    assert (about_us_link.location['y'] + about_us_link.size['height'] <
+            contact_link.location['y'] <
+            about_us_link.location['y'] + 2 * about_us_link.size['height'])
     # On the left
-    assert about_us.location['x'] + about_us.size['width'] < selenium.get_window_size()['width']/2
+    assert (about_us_link.location['x'] + about_us_link.size['width'] <
+            selenium.get_window_size()['width']/2)
+
+@markers.webview
+@markers.nondestructive
+def test_about_us_content_includes_openstax_goals(base_url, selenium):
+    # GIVEN the home page
+    home = Home(selenium, base_url).open()
+
+    # WHEN the About Us link in the navbar is clicked
+    about_us = home.header.click_about_us()
+
+    # THEN the content includes a paragraph about the goals of OpenStax
+    assert ('Today, OpenStax CNX is a dynamic non-profit digital ecosystem serving '
+            'millions of users per month in the delivery of educational content '
+            'to improve learning outcomes.') in about_us.about_content
