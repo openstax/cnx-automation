56b3070e98ae8815e5014ccbaf97ff7bcd74491b
Dante Soares <dante.m.soares@rice.edu>
2018-05-24 18:34:13 -0500

    Added tests for social share links

--- tests/webview/ui/test_content.py
+++ tests/webview/ui/test_content.py
@@ -4,7 +4,44 @@
 
 from tests import markers
 
+from pages.webview.home import Home
 from pages.webview.content import Content
+
+
+@markers.webview
+@markers.nondestructive
+def test_share_on_top_right_corner(base_url, selenium):
+    # GIVEN the home page
+    home = Home(selenium, base_url).open()
+
+    # WHEN a book is clicked
+    book = home.featured_books.openstax_list[0]
+    content = book.click_book_cover()
+
+    # THEN social share links are displayed in the top right corner
+    root = content.share.root
+    # Top half
+    assert root.location['y'] + root.size['height'] < selenium.get_window_size()['height']/2
+    # Right half
+    assert root.location['x'] > selenium.get_window_size()['width']/2
+
+
+@markers.webview
+@markers.nondestructive
+def test_share_links_displayed(base_url, selenium):
+    # GIVEN the home page
+    home = Home(selenium, base_url).open()
+
+    # WHEN a book is clicked
+    book = home.featured_books.openstax_list[0]
+    content = book.click_book_cover()
+
+    # THEN social share links are displayed with the expected urls
+    share = content.share
+    assert share.is_facebook_share_link_displayed
+    assert share.is_twitter_share_link_displayed
+    assert share.is_google_share_link_displayed
+    assert share.is_linkedin_share_link_displayed
 
 
 @markers.webview
