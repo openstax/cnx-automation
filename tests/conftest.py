5796d9267de050823c70e01a380298d1513b4f25
Dante Soares <dante.m.soares@rice.edu>
2018-06-04 10:35:12 -0500

    Added monkeypatch to fix Connection Reset By Peer

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,12 @@ import pytest
 
 from dotenv import load_dotenv
 
+DOTENV_PATH = os.path.join(
+  os.path.realpath(os.path.dirname(__file__)), '../.env')
+load_dotenv(dotenv_path=DOTENV_PATH)
+
+
+
 # Import fixtures from our package so pytest can detect them
 from fixtures.base import chrome_options, selenium # flake8: noqa
 from fixtures.snapshot import snapshot
@@ -14,18 +20,12 @@ from fixtures.archive import archive_base_url
 from fixtures.webview import american_gov_url, content_url
 from fixtures.legacy import (legacy_base_url, legacy_username, legacy_password,
                              m46922_1_13_cnxml_filepath)
-from tests.utils import patch_module
+from patches import connection_reset_by_peer
 
 # Load environment variables from .env file
 DOTENV_PATH = os.path.join(
   os.path.realpath(os.path.dirname(__file__)), '../.env')
 load_dotenv(dotenv_path=DOTENV_PATH)
-
-# Patch remote_connection to workaround Connection Reset by Peer bug in the Selenium driver
-# https://github.com/SeleniumHQ/selenium/issues/5296
-patch_module('patches.remote_connection',
-             'selenium.webdriver.remote.remote_connection',
-             '__init__')
 
 
 def pytest_addoption(parser):
