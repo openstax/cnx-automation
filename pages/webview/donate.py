0ca19f6bded86867945c3369c8f5243ff1bc2d73
Dante Soares <dante.m.soares@rice.edu>
2018-06-06 11:00:36 -0500

    Added a test for the donation slider

--- pages/webview/donate.py
+++ pages/webview/donate.py
@@ -2,9 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from pages.webview.base import Page
+from regions.webview.base import Region
 
 
 class Donate(Page):
@@ -15,10 +19,50 @@ class Donate(Page):
         return self.find_element(*self._form_locator)
 
     @property
+    def donation_slider(self):
+        return self.DonationSlider(self, self.form)
+
+    @property
+    def donation_value(self):
+        return self.donation_slider.donation_value
+
+    @property
     def loaded(self):
         return self.is_element_displayed(*self._form_locator)
 
     def submit(self):
-        self.form.submit()
-        from pages.webview.donate_form import DonateForm
-        return DonateForm(self.driver, self.base_url, self.timeout).wait_for_page_to_load()
+        return self.donation_slider.submit()
+
+    class DonationSlider(Region):
+        _range_input_locator = (By.CSS_SELECTOR, 'input[type="range"][name="donation"]')
+        _donation_value_span_locator = (By.CSS_SELECTOR, 'span.donation-value')
+
+        @property
+        def range_input(self):
+            return self.find_element(*self._range_input_locator)
+
+        @property
+        def range_input_value(self):
+            return int(self.range_input.get_attribute('aria-valuenow'))
+
+        @property
+        def donation_value_span(self):
+            return self.find_element(*self._donation_value_span_locator)
+
+        @property
+        def donation_value(self):
+            return json.loads(self.donation_value_span.get_attribute('data-l10n-args'))['amount']
+
+        def move_left(self):
+            self.range_input.send_keys(Keys.LEFT)
+            return self
+
+        def move_right(self):
+            self.range_input.send_keys(Keys.RIGHT)
+            return self
+
+        def submit(self):
+            self.root.submit()
+            from pages.webview.donate_form import DonateForm
+            donate_form = DonateForm(self.driver, self.page.base_url, self.page.timeout)
+            return donate_form.wait_for_page_to_load()
--- tests/webview/ui/test_donate.py
+++ tests/webview/ui/test_donate.py
@@ -5,6 +5,7 @@
 from tests import markers
 
 from pages.webview.home import Home
+from pages.webview.donate import Donate
 from pages.webview.donate_form import DonateForm
 
 
@@ -43,3 +44,31 @@ def test_donate_form_incomplete(base_url, selenium):
     # due to the HTML5 "required" attribute
     # Instead, we check that there are required inputs in the page
     assert len(donate_form.required_inputs) > 0
+
+
+@markers.webview
+@markers.nondestructive
+def test_donate_slider(base_url, selenium):
+    # GIVEN the home page
+    home = Home(selenium, base_url).open()
+
+    # WHEN the donate link is clicked and then the donate now button is clicked
+    donate = home.header.click_donate()
+    donation_slider = donate.donation_slider
+
+    # THEN the donation slider can be dragged around and correct donation values are displayed
+    assert type(donate) is Donate
+    donation_values = [5, 10, 15, 20, 25, 50, 75, 100, 250, 500, 1000, 2500, 5000, 10000]
+    starting_index = 1
+    assert donation_slider.range_input_value == donation_slider.donation_value
+    assert donation_slider.donation_value == donation_values[starting_index]
+
+    for i in range(starting_index):
+        donation_slider.move_left()
+        assert donation_slider.range_input_value == donation_slider.donation_value
+        assert donation_slider.donation_value == donation_values[starting_index - i - 1]
+
+    for donation_value in donation_values[1:]:
+        donation_slider.move_right()
+        assert donation_slider.range_input_value == donation_slider.donation_value
+        assert donation_slider.donation_value == donation_value
