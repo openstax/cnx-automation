ac45fa5ce6d42e4bfb224f60cb94287ffc90e841
m1yag1 <mike.arbelaez@rice.edu>
2018-06-07 13:17:37 -0500

    Import fixture refactor (#124)
    * import fixtures a better way
    * ignore unused import for patch

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,17 +7,18 @@ import pytest
 
 from dotenv import load_dotenv
 
-# Import fixtures from our package so pytest can detect them
-from fixtures.base import chrome_options, selenium # flake8: noqa
-from fixtures.snapshot import snapshot
-from fixtures.archive import archive_base_url
-from fixtures.webview import american_gov_url, content_url
-from fixtures.legacy import (legacy_base_url, legacy_username, legacy_password,
-                             m46922_1_13_cnxml_filepath)
-
 # Patch remote_connection to workaround Connection Reset by Peer bug in the Selenium driver
 # https://github.com/SeleniumHQ/selenium/issues/5296
-from patches import connection_reset_by_peer
+from patches import connection_reset_by_peer  # noqa
+
+# Import fixtures
+pytest_plugins = (
+    'fixtures.base',
+    'fixtures.snapshot',
+    'fixtures.archive',
+    'fixtures.webview',
+    'fixtures.legacy',
+)
 
 # Load environment variables from .env file
 DOTENV_PATH = os.path.join(
