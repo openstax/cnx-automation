c4813d5f90587b29f2028c67fe6999934a4d9ba8
Dante Soares <dante.m.soares@rice.edu>
2018-06-12 15:24:17 -0500

    Added test for neb get

--- cli/neb.py
+++ cli/neb.py
@@ -3,13 +3,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import re
-
 import subprocess
+from contextlib import contextmanager
+from os.path import join, isdir
+from shutil import rmtree
 
 
 # Property class methods: https://stackoverflow.com/a/5189765
 class MetaNeb(type):
     _version_regex = re.compile('^Nebuchadnezzar (.*)$')
+    _tmp_dir = '/tmp'
 
     def invoke(cls, *args, check=True):
         return subprocess.run(['neb', *args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
@@ -23,14 +26,27 @@ class MetaNeb(type):
     def version(cls):
         return cls._version_regex.match(cls.invoke('--version'))[1]
 
-    def get(cls, *, help=False, env=None, col_id=None, col_version=None):
+    @contextmanager
+    def get(cls, *, help=False, verbose=False, env=None, col_id=None, col_version=None):
         if help:
-            return cls.invoke('get', '--help')
+            yield cls.invoke('get', '--help')
+            return
         elif env is None or col_id is None or col_version is None:
             raise(TypeError("get() missing either 1 required keyword-only argument: 'help' or 3"
                             " required keyword-only arguments: 'env', 'col_id', and 'col_version'"))
 
-        return cls.invoke('get', env, col_id, col_version)
+        options = []
+        if verbose:
+            options.append('--verbose')
+
+        dir = join(cls._tmp_dir, col_id)
+
+        try:
+            cls.invoke(*options, 'get', '--output-dir', dir, env, col_id, col_version)
+            yield dir
+        finally:
+            if isdir(dir):
+                rmtree(dir)
 
 
 class Neb(metaclass=MetaNeb):
--- tests/neb/ui/test_get.py
+++ tests/neb/ui/test_get.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from os.path import isdir
+
 from tests import markers
 
 from cli.neb import Neb
@@ -26,9 +28,20 @@ def test_get_help():
     # GIVEN neb
 
     # WHEN we run `neb get --help`
-    help = Neb.get(help=True)
+    with Neb.get(help=True) as help:
+        # THEN the usage message is displayed
+        assert 'Usage: neb get ' in help
+        assert 'Options:' in help
+        assert '--help' in help
 
-    # THEN the usage message is displayed
-    assert 'Usage: neb get ' in help
-    assert 'Options:' in help
-    assert '--help' in help
+
+@markers.neb
+@markers.nondestructive
+@markers.parametrize("col_id,col_version", [('col11758', '1.9')])
+def test_get_col(neb_env, col_id, col_version):
+    # GIVEN neb, an environment name, a collection id and a collection version
+
+    # WHEN we run `neb --verbose get env col_id col_version`
+    with Neb.get(verbose=True, env=neb_env, col_id=col_id, col_version=col_version) as zip_dir:
+        # THEN the complete zip is downloaded
+        assert isdir(zip_dir)
