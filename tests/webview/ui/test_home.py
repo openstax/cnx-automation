b3f2f1a51edd7c17959964448616f717490ced9f
Dante Soares <dante.m.soares@rice.edu>
2018-07-02 13:25:21 -0500

    Small optimization for the 3 home page book tests so we don't reload the home page on the last iteration

--- tests/webview/ui/test_home.py
+++ tests/webview/ui/test_home.py
@@ -124,8 +124,9 @@ def test_read_more_loads_correct_page(webview_base_url, selenium):
         # THEN The book title from the home page matches the content page title
         assert book_title == content_page.title
 
-        selenium.back()
-        home = home.wait_for_page_to_load()
+        if i < _number_of_tested_books - 1:
+            selenium.back()
+            home = home.wait_for_page_to_load()
 
 
 @markers.xfail(reason='https://trello.com/c/mFRaZRqK', raises=AssertionError)
@@ -148,8 +149,9 @@ def test_book_cover_loads_correct_page(webview_base_url, selenium):
         # THEN The book title from the home page matches the content page title
         assert book_title == content_page.title
 
-        selenium.back()
-        home = home.wait_for_page_to_load()
+        if i < _number_of_tested_books - 1:
+            selenium.back()
+            home = home.wait_for_page_to_load()
 
 
 @markers.xfail(reason='https://trello.com/c/mFRaZRqK', raises=AssertionError)
@@ -172,8 +174,9 @@ def test_title_link_loads_correct_page(webview_base_url, selenium):
         # THEN The book title from the home page matches the content page title
         assert book_title == content_page.title
 
-        selenium.back()
-        home = home.wait_for_page_to_load()
+        if i < _number_of_tested_books - 1:
+            selenium.back()
+            home = home.wait_for_page_to_load()
 
 
 @markers.webview
