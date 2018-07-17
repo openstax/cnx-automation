bbee3f855fec3c4ec3479b4a2d9f87773a2bf5e4
Brian <yixuanbrian@hotmail.com>
2018-07-17 16:19:48 -0500

    add decorator to content_page book_list

--- pages/webview/content_page.py
+++ pages/webview/content_page.py
@@ -4,6 +4,7 @@ from tests.utils import retry_stale_element_reference_exception
 
 from pages.webview.content import Content
 from regions.webview.base import Region
+from tests.utils import retry_stale_element_reference_exception
 
 
 class ContentPage(Content):
@@ -24,6 +25,7 @@ class ContentPage(Content):
         _root_locator = (By.CLASS_NAME, 'booksContaining')
         _book_list_locator = (By.CSS_SELECTOR, 'div > ul > li')
 
+        @retry_stale_element_reference_exception
         @property
         def book_list(self):
             return [self.Book(self.page, el) for el in self.find_elements(*self._book_list_locator)]
