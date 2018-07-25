1ce3a9222e2c058e92bfe8a1f175a0edae7631ff
m1yag1 <mike.arbelaez@rice.edu>
2018-07-25 09:58:26 -0500

    fix random import

--- tests/utils.py
+++ tests/utils.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import time
 from functools import wraps
-from random import random
+import random
 
 from selenium.common.exceptions import StaleElementReferenceException
 
