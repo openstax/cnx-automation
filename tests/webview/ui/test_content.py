62b53f4d3cc4e8ef57bbb8de303d88a26050fb25
m1yag1 <mike.arbelaez@rice.edu>
2018-12-05 22:23:43 -0600

    for book title and highlight check that content_page_title in the section title

--- tests/webview/ui/test_content.py
+++ tests/webview/ui/test_content.py
@@ -1059,5 +1059,5 @@ def test_book_title_link_and_highlight_on_view(webview_base_url, id, selenium, h
     active_color = content.table_of_contents.active_page_color
 
     # THEN make sure the section matches the original page title and the highlight color is correct
-    assert content_page_title == content.section_title
+    assert content_page_title in content.section_title
     assert active_color == highlight_color
