24068cf462ced18c9d3ef778df22891babd5835c
m1yag1 <mike.arbelaez@rice.edu>
2019-03-05 12:13:34 -0600

    simplified subprocess command to print output

--- .ci/scripts/run_tests.py
+++ .ci/scripts/run_tests.py
@@ -3,41 +3,33 @@ import json
 import os
 import subprocess
 
-print(os.getcwd())
 
-with open("history/urls.json", "r") as infile:
+def run(cmd):
+    # https://stackoverflow.com/questions/4417546
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True
+    )
+
+    for line in process.stdout:
+        print(line, end="")
+
+    output = process.communicate()[0]
+    exit_code = process.returncode
+
+    return exit_code, output
+
+
+with open("./history/urls.json", "r") as infile:
     urls = json.load(infile)
+
+# Change to cnx-automation resource directory
+os.chdir("./cnx-automation")
 
 os.environ["PYTHONUNBUFFERED"] = "1"
 os.environ["WEBVIEW_BASE_URL"] = urls["webview_url"]
 os.environ["LEGACY_BASE_URL"] = urls["legacy_url"]
 os.environ["ARCHIVE_BASE_URL"] = urls["archive_url"]
 
-
-def run(cmd):
-    proc = subprocess.Popen(
-        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
-    )
-    stdout = []
-    stderr = []
-    mix = []
-
-    while proc.poll() is None:
-        line = proc.stdout.readline()
-        if line != "":
-            stdout.append(line)
-            mix.append(line)
-            print(line, end="")
-
-        line = proc.stderr.readline()
-        if line != "":
-            stderr.append(line)
-            mix.append(line)
-            print(line, end="")
-
-    return proc.returncode, stdout, stderr, mix
-
-
-code, out, err, mix = run("make test-webview")
+code, output = run("make test-webview")
 
 exit(code)
