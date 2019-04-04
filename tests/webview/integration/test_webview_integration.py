f2d3d5717d7ba82f892fd088616751a4ed79a234
BryanEliDimas <bryanelidimas@gmail.com>
2019-04-04 11:58:26 -0500

    Test google redirect against correct server
    Since we're writing this test when the feature may not have been deployed yet, test against correct server.

--- tests/webview/integration/test_webview_integration.py
+++ tests/webview/integration/test_webview_integration.py
@@ -6,7 +6,7 @@ from tests import markers
 @markers.webview
 @markers.nondestructive
 @markers.parametrize("uuid, version", [("fea3130c-6e57-41b2-a00f-0267ffae273c", 5)])
-def test_google_redirect_archive_results(archive_base_url, uuid, version):
+def test_archive_redirect_for_google_results(archive_base_url, uuid, version):
     # end-to-end integration test for redirecting google away from archive URLs
     # see: https://github.com/openstax/cnx/issues/209
 
@@ -16,7 +16,7 @@ def test_google_redirect_archive_results(archive_base_url, uuid, version):
     # WHEN a web request comes from Google
     response = requests.get(url=url, headers={"Referer": "https://google.com/"})
 
-    # THEN we (via Varnish) redirect to the canonical URL (served by webview)
-    # assert "archive" not in response.headers["Location"]
+    # THEN we redirect to the canonical URL (which is served by webview)
+    assert "archive" not in response.headers["Location"]
     assert response.headers["Location"] == response.headers["X-Varnish-Canonical-Url"]
     assert response.headers["X-Varnish-Status"] == "uncacheable - redirected google"
