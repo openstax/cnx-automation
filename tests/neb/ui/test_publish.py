4fa4efc24345bb316411cb0647f396efe09b8bc9
BryanEliDimas <bryanelidimas@gmail.com>
2019-04-05 13:11:12 -0500

    Fix neb tests when no commit message and minor typos

--- tests/neb/ui/test_publish.py
+++ tests/neb/ui/test_publish.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import subprocess
 from tempfile import TemporaryDirectory
 
 from tests import markers
@@ -18,9 +19,9 @@ def test_publish_help():
     # WHEN we run `neb publish --help`
     with Neb.publish(help=True) as help:
         # THEN the usage message is displayed
-        assert 'Usage: neb publish ' in help
-        assert 'Options:' in help
-        assert '--help' in help
+        assert "Usage: neb publish " in help
+        assert "Options:" in help
+        assert "--help" in help
 
 
 @markers.neb
@@ -29,12 +30,12 @@ def test_publish_no_env():
     # GIVEN neb
 
     # WHEN we run `neb publish`
-    stdout, stderr, returncode = Neb.run('publish')
+    stdout, stderr, returncode = Neb.run("publish")
 
     # THEN neb exits with an error and the usage message is displayed
     assert returncode > 0
-    assert 'Usage: neb publish ' in stderr
-    assert 'Error: Missing argument "env"' in stderr
+    assert "Usage: neb publish " in stderr
+    assert 'Error: Missing argument "ENV"' in stderr
 
 
 @markers.neb
@@ -43,39 +44,48 @@ def test_publish_no_col_id(neb_env):
     # GIVEN neb and an environment
 
     # WHEN we run `neb publish env`
-    stdout, stderr, returncode = Neb.run('publish', neb_env)
+    stdout, stderr, returncode = Neb.run("publish", neb_env)
 
     # THEN neb exits with an error and the usage message is displayed
     assert returncode > 0
-    assert 'Usage: neb publish ' in stderr
-    assert 'Error: Missing argument "content_dir"' in stderr
+    assert "Usage: neb publish " in stderr
+    assert 'Error: Missing argument "CONTENT_DIR"' in stderr
 
 
 @markers.neb
 @markers.nondestructive
 def test_publish_no_commit_message(neb_env):
     # GIVEN neb, an environment, and a content dir
-    with TemporaryDirectory() as content_dir:
+    try:
+        with TemporaryDirectory() as content_dir:
 
-        # WHEN we run `neb publish env content_dir`
-        stdout, stderr, returncode = Neb.run('publish', neb_env, content_dir)
-
-    # THEN neb exits with an error and the usage message is displayed
-    assert returncode > 0
-    assert 'Usage: neb publish ' in stderr
-    assert 'Error: Missing argument "publication_message"' in stderr
+            # WHEN we run `neb publish env content_dir`
+            stdout, stderr, returncode = Neb.run("publish", neb_env, content_dir, timeout=1)
+    except subprocess.TimeoutExpired as e:
+        # THEN neb prompts for a publication message and waits for user to enter it until it times out
+        assert "Publication message:" in e.stdout
 
 
 @markers.neb
 @markers.nondestructive
 @markers.slow
 @markers.parametrize(
-    'col_id,col_version,publication_message,expected_validation_errors',
-    [('col11562', '1.19-invalid', 'Made the CNXML invalid',
-      ('/m46885/index.cnxml:102:22 -- error: ID "id9602938" has already been defined',
-       '/m46885/index.cnxml:101:22 -- error: first occurrence of ID "id9602938"'))])
-def test_publish_invalid_cnxml(neb_env, col_id, col_version, publication_message,
-                               expected_validation_errors, snapshot):
+    "col_id,col_version,publication_message,expected_validation_errors",
+    [
+        (
+            "col11562",
+            "1.19-invalid",
+            "Made the CNXML invalid",
+            (
+                '/m46885/index.cnxml:102:22 -- error: ID "id9602938" has already been defined',
+                '/m46885/index.cnxml:101:22 -- error: first occurrence of ID "id9602938"',
+            ),
+        )
+    ],
+)
+def test_publish_invalid_cnxml(
+    neb_env, col_id, col_version, publication_message, expected_validation_errors, snapshot
+):
     # GIVEN neb, an environment, a content dir, a publication message,
     # the expected errors, and the snapshot tool
     snapshot_name = get_neb_snapshot_name(col_id, col_version)
@@ -87,8 +97,9 @@ def test_publish_invalid_cnxml(neb_env, col_id, col_version, publication_message
         # Neb currently fails with a cryptic error if content_dir is not inside the CWD
         # Remove the following 2 lines once https://trello.com/c/jVkv4hQd is fixed
         import os
-        os.chdir(os.path.join(content_dir, '..'))
-        stdout, stderr, returncode = Neb.run('publish', neb_env, content_dir, publication_message)
+
+        os.chdir(os.path.join(content_dir, ".."))
+        stdout, stderr, returncode = Neb.run("publish", neb_env, content_dir, publication_message)
 
     # THEN neb exits with an error and the CNXML validation failure message is displayed
     assert returncode > 0
