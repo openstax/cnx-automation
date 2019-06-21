e0592d10f4ecc546d0850764de0d1280db294519
m1yag1 <mike.arbelaez@rice.edu>
2019-06-21 17:53:10 -0500

    separated fixture into two. one for the token and the other to instantiate applitools

--- fixtures/applitools.py
+++ fixtures/applitools.py
@@ -3,21 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import pytest
-from applitools.eyes import Eyes
+from applitools.selenium import Eyes
 
-@pytest.fixture(scope="session")
-def applitools(request):
+
+@pytest.fixture
+def applitools_api_key(request):
+    config = request.config
+    api_key = config.getoption("--applitools-key")
+
+    if not api_key:
+
+        pytest.warns("No Applitools API Key provided. Visual Tests will fail.")
+
+    return api_key
+
+
+@pytest.fixture
+def applitools(request, applitools_api_key):
     """Returns a client that can interface with the applitools API"""
 
     eyes = Eyes()
+    eyes.api_key = applitools_api_key
 
-    config = request.config
-    eyes.api_key = config.getoption("--applitools-key")
-
-    if not eyes.api_key:
-        raise Exception("Applitools key is missing")
-    else:
-        yield eyes
-
-
-
+    yield eyes
