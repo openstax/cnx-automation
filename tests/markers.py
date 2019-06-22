c94ec11dd16f8c71f683d400b9faf7cda7f4d066
Ottó <44207656+omehes@users.noreply.github.com>
2019-06-21 20:59:42 -0500

    initial commit for new visual test files (#540)
    * created marker for visual tests
    
    * updated travis to not run visual tests
    
    * added fixture for knowing webview instance
    
    * update applitools package
    
    * added test to take full screenshot
    
    * renamed file to group all home page tests
    
    * separated fixture into two. one for the token and the other to instantiate applitools
    
    * trying to fix travis
    
    * moved show more test to home applitools module
    
    * remove width and height parametrize b/c those are already fixtures

--- tests/markers.py
+++ tests/markers.py
@@ -17,6 +17,7 @@ webview = mark.webview
 legacy = mark.legacy
 neb = mark.neb
 rex = mark.rex
+visual = mark.visual
 
 requires_complete_dataset = mark.requires_complete_dataset
 requires_deployment = mark.requires_deployment
