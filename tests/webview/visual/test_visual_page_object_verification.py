988b8de28bfb6d8c355cd2087e572424836af207
Otto Mehes <om9@rice.edu>
2019-07-03 10:07:53 -0500

    changes made: file name

--- tests/webview/visual/test_visual_page_object_verification.py
+++ tests/webview/visual/test_visual_page_object_verification.py
@@ -7,9 +7,9 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.common.exceptions import ElementNotInteractableException
 
-ApplName = 'Random visual page content check'
+ApplName = 'Random pages visual content checking'
 
-# FINAL VERSION: June 20, 2019
+# FINAL VERSION: July 02, 2019
 
 """
 Visually checks pages contents (text, tabs, graphs and images) using applitools' eyes function.
@@ -33,13 +33,13 @@ def test_visual_page_object_verification(applitools, webview_base_url, selenium,
     try:
 
         # Start the test and set the browser's viewport's size
-        applitools.open(driver=selenium, app_name=ApplName, test_name='Random visual page content check', viewport_size={'width': width, 'height': height})
+        applitools.open(driver=selenium, app_name=ApplName, test_name='Page content checks', viewport_size={'width': width, 'height': height})
 
         home = Home(selenium, webview_base_url).open()
         obooks = home.featured_books.openstax_list
 
-        # defines how many books will be checked
-        for i in range(0, len(obooks), 2):
+        # defines how many books will be selected
+        for i in range(0, len(obooks), 4):
 
             home = Home(selenium, webview_base_url).open()
             book = home.featured_books.openstax_list[i]
@@ -47,32 +47,31 @@ def test_visual_page_object_verification(applitools, webview_base_url, selenium,
             ccontent.header_nav.click_contents_button()
             toc = ccontent.table_of_contents
 
-            # navigates to chapters and pages
-            for k in range(1, 10):
+            # navigates to chapters and their sub-chapters
+            for k in range(1, 5):
 
                 try:
-                    # clicks on a number of chapters defined by k in range
+                    # clicks on 'k-1' number of chapters in a book
                     lchapter = toc.chapters[k]
                     cchapter = lchapter.click()
-                    page = cchapter.pages[k]
+                    page = cchapter.pages[1]
                     content = page.click()
-                    content_title = content.clean_title
 
                 except (IndexError, ElementNotVisibleException, StaleElementReferenceException, ElementNotInteractableException) as errors:
 
-                    print("E R R O R  M E S S A G E S : \n")
+                    print("\n E R R O R  M E S S A G E S : \n")
                     print(errors)
-                    print(' -> moving on to next page')
+                    print(' -->> moving on to next page')
 
                 else:
 
-                    # scrolls down on the page
+                    # scrolls down on the selected sub-chapter
                     selenium.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
 
                     sleep(1)
 
-                    # takes screenshot of the scrolled page and stores it in applitools
-                    applitools.check_window('-> {} on {}'.format(content_title, webview_base_url))
+                    # takes screenshot of the scrolled chapter and stores it in applitools
+                    applitools.check_window('-> {}'.format(webview_base_url))
 
         applitools.close()
 
