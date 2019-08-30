337f93b40181825f7fcd1c9f8c44a509e57e2f69
m1yag1 <mike.arbelaez@rice.edu>
2019-08-30 16:38:23 -0500

    added fixture to read in the chem 2e uris

--- fixtures/webview.py
+++ fixtures/webview.py
@@ -4,8 +4,11 @@
 
 import os
 import pytest
+import requests
 
 import tldextract
+from rex_redirects import get_rex_release_json_url
+
 from tests.utils import gen_from_file, skip_if_destructive_and_sensitive
 
 DATA_DIR = os.path.join(os.path.realpath(os.path.dirname(__file__)), "data", "webview")
@@ -18,6 +21,17 @@ def american_gov_uuid(request):
     """Yields American Government UUIDs from the american_gov_uuids.txt file
 
     Example: c6ee95dd-d10b-430c-8a83-20d5a28334a9
+    """
+    yield request.param
+
+
+@pytest.fixture(
+    params=gen_from_file(os.path.join(DATA_DIR, "7fccc9cf-9b71-44f6-800b-f9457fd64335.txt"))
+)
+def chemistry_2e_uri(request):
+    """Yields a URI from the Chemistry 2e book located on a cnx.org instance
+
+    Example: /contents/f8zJz5tx@0.0:2po7o99e/1-essential-ideas
     """
     yield request.param
 
@@ -40,6 +54,17 @@ def rex_base_url(request):
     if base_url is not None:
         skip_if_destructive_and_sensitive(request, base_url)
         return base_url
+
+
+@pytest.fixture
+def rex_released_books(rex_base_url):
+    rex_host = rex_base_url.split("://")[-1]
+
+    release_json_url = get_rex_release_json_url(rex_host)
+    response = requests.get(release_json_url)
+    response.raise_for_status()
+
+    return response.json()["books"]
 
 
 @pytest.fixture
