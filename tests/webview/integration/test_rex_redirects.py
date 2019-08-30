1d795ad16e5e3e055840174067ec71e088df90a1
m1yag1 <mike.arbelaez@rice.edu>
2019-08-30 13:53:08 -0500

    moved and renamed rex redirect file into webview integration tests folder

--- tests/webview/integration/test_rex_redirects.py
+++ tests/webview/integration/test_rex_redirects.py
@@ -50,14 +50,14 @@ def test_redirecting_to_rex_from_within_webview(webview_base_url, rex_base_url, 
 
     # WHEN we click on a featured book "Chemistry 2e"
     for book in home.featured_books.openstax_list:
-        if book.title == 'Chemistry 2e':
+        if book.title == "Chemistry 2e":
             book.offscreen_click(book.book_cover_link)
 
             #  THEN we redirect to REX
             assert rex_base_url in home.current_url
             break
     else:
-        assert False, 'Chemistry 2e not found in featured books'
+        assert False, "Chemistry 2e not found in featured books"
 
 
 @markers.rex
@@ -117,17 +117,17 @@ def test_all_rex_redirects(rex_base_url, archive_base_url, webview_base_url):
     """Use cnx-rex-redirects to generate all cnx urls that should redirect to
     rex and test all of them.
     """
-    rex_host = rex_base_url.split('://')[-1]
-    archive_host = archive_base_url.split('://')[-1]
+    rex_host = rex_base_url.split("://")[-1]
+    archive_host = archive_base_url.split("://")[-1]
 
     # GIVEN the list of books on rex
     release_json_url = get_rex_release_json_url(rex_host)
     release_data = requests.get(release_json_url).json()
-    for book in release_data['books']:
+    for book in release_data["books"]:
         for uri in generate_cnx_uris(archive_host, book):
             # WHEN we go to any page of a rex book on cnx
-            cnx_page_slug = uri.split('/')[-1]
-            cnx_url = f'{webview_base_url}{uri}'
+            cnx_page_slug = uri.split("/")[-1]
+            cnx_url = f"{webview_base_url}{uri}"
             response = requests.get(cnx_url)
 
             # THEN we are redirected to rex
