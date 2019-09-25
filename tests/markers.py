9f864d68364e0567143ebbfb27126be5c051e433
Otto Mehes <om9@rice.edu>
2019-09-25 12:16:00 -0500

    clean up

--- tests/markers.py
+++ tests/markers.py
@@ -13,7 +13,6 @@ nondestructive = mark.nondestructive
 parametrize = mark.parametrize
 test_case = pytestrail.case
 xfail = mark.xfail
-otto = mark.otto
 
 slow = mark.slow
 smoke = mark.smoke
--- tests/webview/integration/test_spreadsheet_redirects.py
+++ tests/webview/integration/test_spreadsheet_redirects.py
@@ -2,25 +2,19 @@ import requests
 from tests import markers
 
 
-@markers.otto
-@markers.test_case("C553080")
+@markers.test_case("C553092")
+@markers.rex
 @markers.nondestructive
 def test_redirecting_to_rex_from_within_webview(
     webview_base_url, rex_base_url, selenium, redirecting_books_titles
 ):
-    """Webview needs to redirect to REX when any of the featured books is a REX book.
+    """Webview needs to redirect to REX when any of the featured books is a REX book/page (list from the spreadsheet).
     https://github.com/openstax/cnx/issues/401
     """
 
     for book in redirecting_books_titles:
         data = requests.get(book)
         url = data.url
-
-        print()
-        print("REDIRECTED FROM: ", book)
-        print("REDIRECTED TO  : ", url)
-        print("STATUS CODE    : ", data.status_code)
-        print()
 
         assert rex_base_url in url
         assert 200 == data.status_code
