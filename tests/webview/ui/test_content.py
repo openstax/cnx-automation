74529075ff3a0a2a103edf48424544e37dc99e6f
Otto Mehes <om9@rice.edu>
2019-09-27 09:33:54 -0500

    some change

--- tests/webview/ui/test_content.py
+++ tests/webview/ui/test_content.py
@@ -320,7 +320,7 @@ def test_share_on_top_right_corner(webview_base_url, selenium):
             None,
             None,
         ),
-        ("e42bd376-624b-4c0f-972f-e0c57998e765", "mitosis genetics", True, 0, True, False),
+        ("e42bd376-624b-4c0f-972f-e0c57998e765", "developing", True, 10, True, False),
         ("d50f6e32-0fda-46ef-a362-9bd36ca7c97d", "table", True, 1, True, True),
     ],
 )
