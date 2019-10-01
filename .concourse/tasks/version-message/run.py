58acda03bcb868bc5378c7b3d1d74c510ce2c478
m1yag1 <mike.arbelaez@rice.edu>
2019-10-01 15:28:07 -0500

    format application versions a bit better ;)

--- .concourse/tasks/version-message/run.py
+++ .concourse/tasks/version-message/run.py
@@ -4,10 +4,21 @@ import os
 
 from string import Template
 
-message_template = """:female-detective: An updated $host_url has been discovered. Browser tests will start in a moment ...
-```
-$version_data
-```
+message_template = """:female-detective: An updated $host_url/history.txt has been discovered. Browser tests will start in a moment ...
+*date*
+$date
+*webview*
+$webview
+*cnx-archive*
+$archive
+*cnx-publishing*
+$publishing
+*oer.exports*
+$oer_exports
+*cnx-press*
+$press
+*cnx-deploy*
+$cnx_deploy
 """
 
 template = Template(message_template)
@@ -25,4 +36,15 @@ with open("urls.json", "r") as infile:
 
 webview_url = urls["webview_url"]
 
-print(template.substitute(host_url=webview_url, version_data=app_versions))
+print(
+    template.substitute(
+        host_url=webview_url,
+        date=app_versions["date"],
+        webview=app_versions["webview"],
+        archive=app_versions["cnx-archive"],
+        publishing=app_versions["cnx-publishing"],
+        oer_exports=app_versions["oer.exports"],
+        press=app_versions["press"],
+        cnx_deploy=app_versions["cnx-deploy"],
+    )
+)
