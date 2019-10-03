f4120e1516bcb3704b4eba5ea8c120c5fb88978a
m1yag1 <mike.arbelaez@rice.edu>
2019-10-03 11:47:48 -0500

    added bash script and task.yml for running functional tests

---
+++ .concourse/tasks/run-functional-tests/script.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+base_dir=$(pwd)
+
+cd history-txt
+
+# Map urls to variables generated from the concourse resource
+WEBVIEW_BASE_URL=$(cat urls.json | jq -r .webview_url)
+ARCHIVE_BASE_URL=$(cat urls.json | jq -r .archive_url)
+LEGACY_BASE_URL=$(cat urls.json | jq -r .legacy_url)
+
+# Send a POST request to circleci with the approriate values
+RESULT=$(curl --header "Content-Type: application/json" \
+         -d '{"build_parameters": {"CIRCLE_JOB": "test-webview",
+                                   "LEGACY_BASE_URL": "'"$LEGACY_BASE_URL"'",
+                                   "WEBVIEW_BASE_URL": "'"$WEBVIEW_BASE_URL"'",
+                                   "ARCHIVE_BASE_URL": "'"$ARCHIVE_BASE_URL"'"}
+             }' https://circleci.com/api/v1.1/project/github/openstax/cnx-automation/tree/master\?circle-token\=$CIRCLE_API_TOKEN)
+
+BUILD_PARAMS=$(echo $RESULT | jq -r .build_parameters)
+
+echo "Build Parameters="$BUILD_PARAMS
+
+BUILD_URL=$(echo "$RESULT" | jq -r .build_url | tee $base_dir/build_url)
+
+echo "Circle CI Build URL=$BUILD_URL"
