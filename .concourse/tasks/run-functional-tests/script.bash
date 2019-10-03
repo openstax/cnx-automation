85ca30cf1c7e914e2b21767b04c063bb8f81a5f9
m1yag1 <mike.arbelaez@rice.edu>
2019-10-03 12:02:40 -0500

    removed useless use of cat :cat:

--- .concourse/tasks/run-functional-tests/script.bash
+++ .concourse/tasks/run-functional-tests/script.bash
@@ -6,9 +6,9 @@ base_dir=$(pwd)
 cd history-txt
 
 # Map urls to variables generated from the concourse resource
-WEBVIEW_BASE_URL=$(cat urls.json | jq -r .webview_url)
-ARCHIVE_BASE_URL=$(cat urls.json | jq -r .archive_url)
-LEGACY_BASE_URL=$(cat urls.json | jq -r .legacy_url)
+WEBVIEW_BASE_URL=$(jq -r .webview_url < urls.json)
+ARCHIVE_BASE_URL=$(jq -r .archive_url < urls.json)
+LEGACY_BASE_URL=$(jq -r .legacy_url < urls.json)
 
 # Send a POST request to circleci with the approriate values
 RESULT=$(curl --header "Content-Type: application/json" \
@@ -18,9 +18,9 @@ RESULT=$(curl --header "Content-Type: application/json" \
                                    "ARCHIVE_BASE_URL": "'"$ARCHIVE_BASE_URL"'"}
              }' https://circleci.com/api/v1.1/project/github/openstax/cnx-automation/tree/master\?circle-token\=$CIRCLE_API_TOKEN)
 
-BUILD_PARAMS=$(echo $RESULT | jq -r .build_parameters)
+BUILD_PARAMS=$(echo "$RESULT" | jq -r .build_parameters)
 
-echo "Build Parameters="$BUILD_PARAMS
+echo "Build Parameters=$BUILD_PARAMS"
 
 BUILD_URL=$(echo "$RESULT" | jq -r .build_url | tee $base_dir/build_url)
 
