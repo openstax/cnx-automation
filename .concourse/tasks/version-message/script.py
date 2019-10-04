6ad0ba436f46621688ac10e17c517dece15ca1ef
m1yag1 <mike.arbelaez@rice.edu>
2019-10-04 16:25:30 -0500

    Refactor functional test pipeline to defer running tests to circle-ci (#618)
    * added bash script and task.yml for running functional tests
    * update version-message to use circleci task
    * added circleci task as input
    * properly set circleci as output
    * remove functional test task for other environments
    * remove unnecessary lines from scripts

--- .concourse/tasks/version-message/script.py
+++ .concourse/tasks/version-message/script.py
@@ -4,8 +4,9 @@ import os
 
 from string import Template
 
-message_template = """:female-detective: An updated $host_url/history.txt has been discovered. Browser tests will start in a moment ...
-*Versions*
+message_template = """:female-detective: An updated $host_url/history.txt has been discovered.
+Tests have been started in CircleCI. Check progress here $circle_url
+*version.txt output*
 ```
 date:           $date
 webview         $webview
@@ -19,16 +20,14 @@ cnx-deploy      $cnx_deploy
 
 template = Template(message_template)
 
-current_dir = os.getcwd()
-history_txt_dir = os.path.join(current_dir, "history-txt")
-
-os.chdir(os.path.join(current_dir, "history-txt"))
-
-with open("app_versions.json", "r") as infile:
+with open("./history-txt/app_versions.json", "r") as infile:
     app_versions = json.load(infile)
 
-with open("urls.json", "r") as infile:
+with open("./history-txt/urls.json", "r") as infile:
     urls = json.load(infile)
+
+with open("./circleci-output/build_url.txt", "r") as infile:
+    circle_url = infile.read()
 
 webview_url = urls["webview_url"]
 
@@ -42,5 +41,6 @@ print(
         oer_exports=app_versions["oer.exports"],
         press=app_versions["press"],
         cnx_deploy=app_versions["cnx-deploy"],
+        circle_url=circle_url,
     )
 )
