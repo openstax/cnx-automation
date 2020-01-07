3a2a2aa59b5bdabb4cdbf05d2f673e6562624a44
Otto Mehes <om9@rice.edu>
2020-01-07 16:25:10 -0600

    changes as per PR review: comment moved above if, better variable name

--- tests/webview/vendorbooks/test_vendorpages_load.py
+++ tests/webview/vendorbooks/test_vendorpages_load.py
@@ -4,6 +4,7 @@ from pages.webview.home import Home
 import requests
 
 
+@markers.otto2
 @markers.vendor
 @markers.test_case("C593142")
 @markers.nondestructive
@@ -11,14 +12,14 @@ def test_vendor_pages_load(vendor_base_url, selenium, openstax_all_books_uuids):
 
     home = Home(selenium, vendor_base_url).open()
 
-    content1 = Content(selenium, vendor_base_url, id=openstax_all_books_uuids).open()
+    vendor_content = Content(selenium, vendor_base_url, id=openstax_all_books_uuids).open()
 
-    content = content1.open()
+    content = vendor_content.open()
     content.header_nav.click_contents_button()
     toc = content.table_of_contents
 
+    # testing American Government 2e book as it requires different indexing
     if openstax_all_books_uuids == "9d8df601-4f12-4ac1-8224-b450bf739e5f":
-        # testing American Government 2e book as it requires different indexing
 
         # WHEN a chapter is expanded and we navigate to one of its pages
         chapter = toc.chapters[2]
@@ -46,8 +47,8 @@ def test_vendor_pages_load(vendor_base_url, selenium, openstax_all_books_uuids):
         current_url = home.current_url
         data = requests.get(current_url)
 
-        assert vendor_base_url in current_url
-        assert 200 == data.status_code
+        assert vendor_base_url in current_url, "vendor_base_url is NOT in current_url"
+        assert 200 == data.status_code, "status code is NOT 200"
 
     else:
         # checking rest of the books that title and chapter/pages redirects
