2045adeed6a12f588acd0c88afaccf9138a3f1f1
m1yag1 <mike.arbelaez@rice.edu>
2020-03-27 11:37:53 -0500

    Revert "Updated the name of the test and made headers a variable * Updated the name of the test to test_location_header_applied_to_redirect to better convey the intent of the test. * Made the headers a dictionary and used the key in the assertion for comparing the value."
    This reverts commit 44e5c2da2e6b3037c3290ef100d8698100b14830.

--- tests/archive/integration/test_redirects.py
+++ tests/archive/integration/test_redirects.py
@@ -6,13 +6,12 @@ from tests import markers
 @markers.archive
 @markers.nondestructive
 @markers.parametrize("uuid", [("fea3130c-6e57-41b2-a00f-0267ffae273c")])
-def test_location_header_applied_to_redirect(archive_base_url, uuid):
-    # GIVEN an "archive" URL and Host Header
+def test_host_header_redirect(archive_base_url, uuid):
+    # GIVEN an "archive" URL
     url = f"{archive_base_url}/contents/{uuid}"
-    headers = {"Host": "openstax.org"}
 
     # WHEN a web request has a host
-    response = requests.get(url=url, headers=headers, allow_redirects=False)
+    response = requests.get(url=url, headers={"Host": "openstax.org"}, allow_redirects=False)
 
-    # THEN the host value is used in the Location header of the redirect
-    assert headers["Host"] in response.headers["Location"]
+    # THEN the host is used in the redirect
+    assert "openstax.org" in response.headers["Location"]
