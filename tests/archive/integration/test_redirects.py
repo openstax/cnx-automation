91b2fabb24eb4e5f53371282fd6ea12b82340298
m1yag1 <mike.arbelaez@rice.edu>
2020-03-27 11:37:53 -0500

    Revert "move test_headers_for_google results to archive application folder"
    This reverts commit 68690634d32e896f8ee31471c31f83b338fbecd2.

--- tests/archive/integration/test_redirects.py
+++ tests/archive/integration/test_redirects.py
@@ -16,23 +16,3 @@ def test_location_header_applied_to_redirect(archive_base_url, uuid):
 
     # THEN the host value is used in the Location header of the redirect
     assert headers["Host"] in response.headers["Location"]
-
-
-@markers.archive
-@markers.webview
-@markers.nondestructive
-@markers.parametrize("uuid, version", [("fea3130c-6e57-41b2-a00f-0267ffae273c", 5)])
-def test_headers_for_google_results(archive_base_url, uuid, version):
-    # end-to-end integration test for redirecting google away from archive URLs
-    # see: https://github.com/openstax/cnx/issues/209
-
-    # GIVEN an "archive" URL
-    url = f"{archive_base_url}/contents/{uuid}@{version}"
-
-    # WHEN a web request comes from Google
-    response = requests.get(url=url, headers={"Referer": "https://google.com/"})
-
-    # THEN we redirect to the canonical URL (which is served by webview)
-    assert "archive" not in response.headers["Location"]
-    assert response.headers["Location"] == response.headers["X-Varnish-Canonical-Url"]
-    assert response.headers["X-Varnish-Status"] == "uncacheable - redirected google"
