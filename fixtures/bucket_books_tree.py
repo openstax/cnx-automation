79c72976c6aff3d9da4b9d7c9a59830d747da0f3
Otto Mehes <om9@rice.edu>
2020-10-07 14:50:09 -0500

    adding code_tag instead of fixed master, modifying exception handling

--- fixtures/bucket_books_tree.py
+++ fixtures/bucket_books_tree.py
@@ -4,16 +4,18 @@ import json
 import urllib
 from urllib.request import urlopen
 
+from urllib.error import HTTPError
+
 """
-Verifies which collections are present in the aws s3 bucket
-Latest update on Aug. 6th, 2020
+Returns the content tree of each collection in aws s3 bucket
+Latest update on Oct. 7th, 2020
 """
 
 
 @pytest.fixture
-def bucket_books_tree(s3_base_url, s3_queue_state_bucket_books):
+def bucket_books_tree(s3_base_url, code_tag, s3_queue_state_bucket_books):
 
-    s3_archive_folder = "/apps/archive/master/contents/"
+    s3_archive_folder = f"/apps/archive/{code_tag}/contents/"
 
     # reading nested lists and getting slug names
     json_data = json.loads(s3_queue_state_bucket_books)
@@ -39,13 +41,27 @@ def bucket_books_tree(s3_base_url, s3_queue_state_bucket_books):
         f"{s3_base_url}{s3_archive_folder}{i}@{j}.json" for i, j in zip(uuid_list, version_list)
     ]
 
+    # list of approved book urls without http errors
+    approved_books_full_url_modified = []
+
+    for ublu in approved_books_full_url:
+
+        try:
+            urllib.request.urlopen(ublu)
+        except HTTPError as h_e:
+            # Return code 404, 501, ...
+            print("HTTPError (check concourse jobs): {}".format(h_e.code) + ", " + ublu)
+        else:
+            # Return code 200
+            approved_books_full_url_modified.append(ublu)
+
     book_tree = []
 
-    for books in range(len(approved_books_full_url)):
+    for books in range(len(approved_books_full_url_modified)):
 
-        s3_books = urllib.request.urlopen(approved_books_full_url[books]).read()
-
+        s3_books = urllib.request.urlopen(approved_books_full_url_modified[books]).read()
         s3_jdata = json.loads(s3_books)
         book_tree.append(s3_jdata["tree"])
+        continue
 
     return book_tree
