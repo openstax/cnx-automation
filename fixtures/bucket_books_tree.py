41f0dd50be4e174341c9a4eee3c6606b0a3ac0b6
Otto Mehes <om9@rice.edu>
2020-10-14 13:31:54 -0500

    adjusting the fixture code as per Ripal's comment

--- fixtures/bucket_books_tree.py
+++ fixtures/bucket_books_tree.py
@@ -8,7 +8,7 @@ from urllib.error import HTTPError
 
 """
 Returns the content tree of each collection in aws s3 bucket
-Latest update on Oct. 7th, 2020
+Latest update on Oct. 14th, 2020
 """
 
 
@@ -37,26 +37,20 @@ def bucket_books_tree(s3_base_url, code_tag, s3_queue_state_bucket_books):
     ]
 
     # list of approved book urls without http errors
-    approved_books_full_url_modified = []
-
-    for ublu in approved_books_full_url:
-
-        try:
-            urllib.request.urlopen(ublu)
-        except HTTPError as h_e:
-            # Return code 404, 501, ...
-            print("HTTPError (check concourse jobs): {}".format(h_e.code) + ", " + ublu)
-        else:
-            # Return code 200
-            approved_books_full_url_modified.append(ublu)
-
     book_tree = []
 
-    for books in range(len(approved_books_full_url_modified)):
+    for books in approved_books_full_url:
 
-        s3_books = urllib.request.urlopen(approved_books_full_url_modified[books]).read()
-        s3_jdata = json.loads(s3_books)
-        book_tree.append(s3_jdata["tree"])
-        continue
+        try:
+            urllib.request.urlopen(books)
+        except HTTPError as h_e:
+            # Return code 404, 501, ...
+            print("HTTPError (check concourse jobs): {}".format(h_e.code) + ", " + books)
+        else:
+            # Return code 200
+            s3_books = urllib.request.urlopen(books).read()
+            s3_jdata = json.loads(s3_books)
+            book_tree.append(s3_jdata["tree"])
+            continue
 
     return book_tree
