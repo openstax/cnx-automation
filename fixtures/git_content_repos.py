4fa564868569a9e5bc6610af0aea4a1e8eed7393
Otto Mehes <om9@rice.edu>
2021-03-26 16:14:19 -0500

    amendments to the fixture for the github repo tests

--- fixtures/git_content_repos.py
+++ fixtures/git_content_repos.py
@@ -1,10 +1,28 @@
 import pytest
+import json
+import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
 
 
 @pytest.fixture
-def git_content_repos():
-    """Returns the list of all github content repos
+def git_content_repos(headers_data):
+
+    """Returns the list of all github content repos (starting with osbooks-)
     """
-    repo = ["osbooks-university-physics", "osbooks-college-algebra-bundle"]
+
+    repos_url = "https://api.github.com/orgs/openstax/repos?per_page=100&type=private"
+
+    repos_req = urllib.request.Request(repos_url, headers=headers_data)
+    repos_list = urllib.request.urlopen(repos_req).read()
+
+    repo = []
+
+    for item in json.loads(repos_list):
+        for key, val in item.items():
+            if str(val).startswith("osbooks-"):
+                name_list = item["name"]
+                repo.append(name_list)
 
     return repo
