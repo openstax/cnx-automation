b515527a908cf10c010286521960c2e53e0cc4ca
Otto Mehes <om9@rice.edu>
2021-09-10 15:53:14 -0500

    adding helper function as per Ripal's comment

--- tests/webview/integration/test_rex_redirects5.py
+++ tests/webview/integration/test_rex_redirects5.py
@@ -12,6 +12,16 @@ def get_url(url):
     return requests.get(url)
 
 
+def log_failures_to_csv(status_code, cnx_url):
+    dict_urls_errors = {"ERROR": status_code, "PAGE URL": cnx_url}
+
+    with open("url_failures_report.csv", "a") as urlerr:
+        writer = csv.DictWriter(urlerr, dict_urls_errors.keys())
+        if urlerr.tell() == 0:
+            writer.writeheader()
+        writer.writerow(dict_urls_errors)
+
+
 @markers.slow
 @markers.nondestructive
 def test_calculus2_uri_redirect_to_rex(webview_base_url, rex_base_url, calculus_vol_2_uri):
@@ -24,16 +34,7 @@ def test_calculus2_uri_redirect_to_rex(webview_base_url, rex_base_url, calculus_
 
     # THEN we are redirected to rex
     if response.status_code != 200:
-
-        dict_urls_errors = {"ERROR": response.status_code, "PAGE URL": cnx_url}
-
-        with open("url_error_list.csv", "a") as urlerr:
-            writer = csv.DictWriter(urlerr, dict_urls_errors.keys())
-
-            if urlerr.tell() == 0:
-                writer.writeheader()
-
-            writer.writerow(dict_urls_errors)
+        log_failures_to_csv(response.status_code, cnx_url)
 
         pytest.fail(f"{response.status_code} in {cnx_page_slug}")
 
