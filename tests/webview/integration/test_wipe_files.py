596cd36b991f094418f9adc2b0b0ac7f5ec2405a
Otto Mehes <om9@rice.edu>
2021-09-13 17:33:15 -0500

    amending the report file name in the wipe file

--- tests/webview/integration/test_wipe_files.py
+++ tests/webview/integration/test_wipe_files.py
@@ -4,7 +4,7 @@ import os
 def test_wipe_files():
     s3_bucket_books = f"{os.getcwd()}/fixtures/data/webview/s3_bucket_books.json"
 
-    with open("url_error_list.csv", "w") as ef, open("report.csv", "w") as rt, open(
+    with open("url_failures_report.csv", "w") as ef, open("report.csv", "w") as rt, open(
         s3_bucket_books, "w"
     ) as s3:
         ef.truncate()
