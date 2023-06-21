0f4c0557fecc818da19d554ec95b626eafa130b7
Otto Mehes <om9@rice.edu>
2023-06-21 16:21:07 -0500

    adjustments as per reviews

--- tests/docx/test_compare_pdf_files.py
+++ tests/docx/test_compare_pdf_files.py
@@ -13,43 +13,40 @@ Compares two pdf files. To run:
 3. make sure that base_to_dir and base_from_dir variables are set correctly
 4. run 'pytest -k test_compare_pdf_files.py tests/docx'
 
-Latest update on June 15th, 2023
+Latest update on June 21st, 2023
 """
 
 
 def test_compare_pdf_files():
-    # Part 1: Copies pdf files into a folder
+    # Part 1: Copies pdf files into predefined folder
 
     base_to_dir = "./pdf_files"
 
     home_dir = os.path.expanduser("~")
     base_from_dir = f"{home_dir}/downloads/"
 
-    shutil.copytree(base_from_dir, base_to_dir, dirs_exist_ok=True)
-
-    all_files = os.listdir(base_to_dir)
-    pdfs_only = [x for x in all_files if x.endswith(".pdf")]
-
     pdf_files_paths = []
 
-    if len(pdfs_only) == 2:
-        for i in range(len(pdfs_only)):
-            file = os.path.join(base_from_dir, pdfs_only[i])
-            tof = os.path.join(base_to_dir, pdfs_only[i])
-            shutil.copy(file, tof)
+    for afile in os.listdir(base_from_dir):
+        if afile.endswith(".pdf") and "-git-" in afile:
+            from_dir = os.path.join(base_from_dir, afile)
+            to_dir = os.path.join(base_to_dir, afile)
 
-            pdf_files_paths.append(tof)
+            shutil.copy(from_dir, to_dir)
 
-    else:
-        pytest.fail(f"Two pdf files required: {len(pdfs_only)} file(s) available")
+            pdf_files_paths.append(to_dir)
 
     # Part 2: Compares pdf files
 
-    try:
-        assert pdf_similar(pdf_files_paths[0], pdf_files_paths[1])
+    if len(pdf_files_paths) == 2:
+        try:
+            assert pdf_similar(pdf_files_paths[0], pdf_files_paths[1])
 
-    except AssertionError:
-        pytest.exit("PDFS ARE NOT THE SAME")
+        except AssertionError:
+            pytest.exit("PDFS ARE NOT THE SAME")
+
+        else:
+            print("\n\n=========>>> PDFS APPEAR TO BE THE SAME")
 
     else:
-        print("\n\n=========>>> PDFS APPEAR TO BE THE SAME")
+        pytest.fail(f"Two pdf files required: {len(pdf_files_paths)} file(s) available")
