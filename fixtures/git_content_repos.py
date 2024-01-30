346b45a9b450e1dd8ac67ed58f2d6eb03895caa2
Otto Mehes <om9@rice.edu>
2024-01-30 11:31:44 -0600

    amending fixture

--- fixtures/git_content_repos.py
+++ fixtures/git_content_repos.py
@@ -19,6 +19,12 @@ def git_content_repos(headers_data):
         "minibook",
         "osbooks-failing-test-book",
         "osbooks-test-content",
+        "osbooks-mikroekonomia",
+        "osbooks-life-liberty-and-pursuit-happiness",
+        "osbooks-fizyka-bundle",
+        "osbooks-psychologia",
+        "osbooks-pl-marketing",
+        "osbooks-makroekonomia",
     ]
 
     next_url = "https://api.github.com/orgs/openstax/repos?per_page=50"
