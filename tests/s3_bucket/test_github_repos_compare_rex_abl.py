dfd4068ec534a0d2aa3c9c0b9f66628df7c194c2
Ottó <om9@rice.edu>
2024-07-30 16:02:30 -0500

    adjusting qa tests for abl api

--- tests/s3_bucket/test_github_repos_compare_rex_abl.py
+++ tests/s3_bucket/test_github_repos_compare_rex_abl.py
@@ -1,22 +1,13 @@
 """
-Compares uuids from rex/release.json and abl
-Latest update on March 27, 2024
+Compares uuids from rex/release.json and abl api
+Latest update on July 30th, 2024
 """
 
 
-def test_github_repos_compare_rex_abl(abl_approved, rex_released_books):
-    abl_uuids_slugs = {}
-
-    for i in abl_approved:
-        book_versions = i["versions"]
-        for j in book_versions:
-            commit_metadata = j["commit_metadata"]["books"]
-            for k in commit_metadata:
-                abl_uuids_slugs[k["uuid"]] = k["slug"]
-
+def test_github_repos_compare_rex_abl(rex_released_books, abl_books_uuids_slugs):
     rex_uuid_list = list(rex_released_books.keys())
 
-    for item in abl_uuids_slugs.items():
+    for item in abl_books_uuids_slugs.items():
         print(f"--->>> Checking {item[1]}:")
 
         try:
