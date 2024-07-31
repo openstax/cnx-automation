10b96d9e22d07a03f83723cab118ec3b0091d44d
Ottó <44207656+omehes@users.noreply.github.com>
2024-07-31 09:22:29 -0500

    adjusting qa tests for abl api (#823)

--- fixtures/abl_books_uuids_slugs.py
+++ fixtures/abl_books_uuids_slugs.py
@@ -2,22 +2,17 @@ import pytest
 
 """
 Creates a dictionary of book uuid:slug for all github content repos.
-Latest update on May 4th, 2022
+Latest update on July 30th, 2024
 """
 
 
 @pytest.fixture
-def abl_books_uuids_slugs(abl_approved):
-
-    """Returns dictionary of uuid:slug values of all collection entries in ABL json"""
+def abl_books_uuids_slugs(abl_api_approved):
+    """Returns dictionary of uuid:slug values of all collection entries in ABL api"""
 
     uuids_slugs = {}
 
-    for i in abl_approved:
-        book_versions = i["versions"]
-        for j in book_versions:
-            commit_metadata = j["commit_metadata"]["books"]
-            for k in commit_metadata:
-                uuids_slugs[k["uuid"]] = k["slug"]
+    for i in abl_api_approved:
+        uuids_slugs[i["uuid"]] = i["slug"]
 
     return uuids_slugs
